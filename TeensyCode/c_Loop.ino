d22849cac798ab8ca881a40c573882b37d63f7c9
Nathan McGuire <nathan.e.mcguire@gmail.com>
2019-12-06 14:47:31 -0500

    Fixed gait generation

--- TeensyCode/c_Loop.ino
+++ TeensyCode/c_Loop.ino
@@ -1,6 +1,11 @@
 //loop
 
 void loop() {
+  if (millis()%1000 > 500) {
+    blinkAllLights(1);
+  } else {
+    blinkAllLights(0);
+  }
   int legPositions [NUM_LEGS] [3];
   for (int i = 0; i < NUM_LEGS; i++) {
     legPositions[i][0] = kInitialPositions[i][0];
@@ -8,5 +13,13 @@ void loop() {
     legPositions[i][2] = kInitialPositions[i][2];
   }
 
-  GaitTripod(legPositions, 10000, millis(), 0, 0, 0, 0);
+  GaitTripod(legPositions, 10000, millis(), 20, 0, 5, 0);
+}
+
+void blinkAllLights(int state){
+  digitalWrite(LED_RUNNING, state);
+  digitalWrite(LED_ERROR, 1-state);
+  digitalWrite(LED_RGB_RED, state);
+  digitalWrite(LED_RGB_GRE, state);
+  digitalWrite(LED_RGB_BLU, 1-state);
 }
--- TeensyCode/d_GaitGeneration.ino
+++ TeensyCode/d_GaitGeneration.ino
@@ -17,21 +17,21 @@ void GaitTripod(int legPositions[NUM_LEGS][3], int period, int currentTime, int 
   }
 
   //Scale waves based on direction of travel
-  float scaledYawWave = (yawVelocity * PI / 4 * squareWave);
+  float scaledYawWave = (yawVelocity * PI / 4.0 * squareWave);
   float scaledXWave = xVelocity * triangularWave;
   float scaledYWave = yVelocity * triangularWave;
-  float scaledZWave = (stepHeight / 2) * squareWave;
+  float scaledZWave = (float(stepHeight) / 2.0) * squareWave;
 
   //Add scaled values to initial leg positions
   for (int i = 0; i < NUM_LEGS; i++) {
-    legPositions[i][0] += scaledXWave;
-    legPositions[i][1] += scaledYWave;
+    float xtemp = legPositions[i][0] + scaledXWave;
+    float ytemp = legPositions[i][1] + scaledYWave;
     legPositions[i][2] += scaledZWave;
 
     //Add in rotation values
-    legPositions[i][0] = (legPositions[i][0] * cosf(scaledYawWave) - output1[i][1] * sinf(scaledYawWave));
-    legPositions[i][1] = (legPositions[i][0] * sinf(scaledYawWave) + output1[i][1] * cosf(scaledYawWave));
+    legPositions[i][0] = (xtemp * cosf(scaledYawWave) - ytemp * sinf(scaledYawWave));
+    legPositions[i][1] = (xtemp * sinf(scaledYawWave) + ytemp * cosf(scaledYawWave));
   }
 
-  //Serial.println(String(squareWave) + ' ' + String(triangularWave));
+  Serial.println(String(legPositions[0][0]) + ' ' + String(legPositions[0][1]) + ' ' + String(legPositions[0][2]));
 }
